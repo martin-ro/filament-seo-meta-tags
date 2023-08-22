0312bd30cf1cb497043a49018abc843e80474bda
Martin Ro <martin-ro@users.noreply.github.com>
2023-08-23 07:03:23 +0800

    Improved configuration

--- src/Components/Forms/SeoTitle.php
+++ src/Components/Forms/SeoTitle.php
@@ -3,27 +3,42 @@
 namespace MartinRo\FilamentSeoMetaTags\Components\Forms;
 
 use Filament\Forms\Components\Textarea;
+use MartinRo\FilamentSeoMetaTags\Concerns\HasHintColor;
 
 class SeoTitle
 {
-    public static function make(string $name = 'title'): Textarea
-    {
+    use HasHintColor;
+
+    public static function make(
+        string $name = 'title',
+        int $minLength = 50,
+        int $maxLength = 70,
+        int $rows = 1,
+        bool $strict = false
+    ): Textarea {
+        if ($strict) {
+            return Textarea::make($name)
+                ->rows($rows)
+                ->minLength($minLength)
+                ->maxlength($maxLength)
+                ->live()
+                ->hint(fn ($state, $component) => strlen($state).'/'.$component->getMaxLength())
+                ->hintColor(fn ($state, $component) => self::getColor(
+                    $state,
+                    $component->getMinLength(),
+                    $component->getMaxLength()
+                ));
+        }
+
         return Textarea::make($name)
-            ->rows(1)
-            ->maxlength(70)
-            ->minLength(50)
-            ->reactive()
-            ->hint(fn ($state, $component) => strlen($state).'/'.$component->getMaxLength())
-            ->hintColor(function ($state, $component) {
-                if (strlen($state) > $component->getMinLength()) {
-                    return 'success';
-                }
-
-                if (strlen($state) > 0) {
-                    return 'danger';
-                }
-
-                return 'gray';
-            });
+            ->rows($rows)
+            ->live()
+            ->hint(fn ($state, $component) => strlen($state).'/'.$component->getExtraInputAttributes()['max-char-count'])
+            ->extraInputAttributes(['min-char-count' => $minLength, 'max-char-count' => $maxLength])
+            ->hintColor(fn ($state, $component) => self::getColor(
+                $state,
+                $component->getExtraInputAttributes()['min-char-count'],
+                $component->getExtraInputAttributes()['max-char-count']
+            ));
     }
 }
