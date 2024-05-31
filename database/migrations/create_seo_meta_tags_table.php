c0f39570cd30c5109a3ff9a0114ef60cbdb95dc6
Martin Ro <martin-ro@users.noreply.github.com>
2024-05-31 09:22:38 +0800

    Check if table exist before running migrations

--- database/migrations/create_seo_meta_tags_table.php
+++ database/migrations/create_seo_meta_tags_table.php
@@ -8,16 +8,18 @@ return new class extends Migration
 {
     public function up()
     {
-        Schema::create('seo_meta_tags', function (Blueprint $table) {
-            $table->id();
+        if (! Schema::hasTable('seo_meta_tags')) {
+            Schema::create('seo_meta_tags', function (Blueprint $table) {
+                $table->id();
 
-            $table->unique(['model_id', 'model_type']);
+                $table->unique(['model_id', 'model_type']);
 
-            $table->morphs('model');
+                $table->morphs('model');
 
-            $table->string('title')->nullable();
-            $table->text('description')->nullable();
-            $table->string('robots')->nullable()->default('index, follow');
-        });
+                $table->string('title')->nullable();
+                $table->text('description')->nullable();
+                $table->string('robots')->nullable()->default('index, follow');
+            });
+        }
     }
 };
